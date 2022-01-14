eb52ab88ef7f09cd38a211d4ab3f85c528ca2b6a
sagun-shrestha <sshrestha426699@gmail.com>
2022-01-13 18:36:23 -0700

    all done i think

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 require('dotenv').config()
 const express = require('express')
-
+const Users = require('./api/model')
 const server = express();
 
 server.use(express.json())
-
-
 
 const PORT = process.env.PORT || 8080
 
 server.listen(PORT, () => {
     console.log('listening on port', PORT)
+})
+
+server.get('/api/user', async (req, res)=> {
+    try{
+        const all = await Users.find()
+        res.json(all)
+    }catch(err){
+        res.status(500).json({message:err})
+    }
+})
+
+server.post('./api/register', async (req, res) => {
+    try{
+        const newUser = await Users.insert(req.body)
+        res.status(201).json(newUser)
+    }catch(err){
+        res.status(500).json({message:err})
+    }
+})
+
+server.post('./api/login', async (req, res) => {
+    try{
+        const user = await Users.findByName()
+        res.json(user.name)
+    }catch(err){
+        res.status(500).json({message:err})
+    }
 })
